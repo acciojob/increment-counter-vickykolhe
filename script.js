d747832c38d4a683f7d9d55026250fc9384b686c
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-08-07 09:10:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 let ptag=document.getElementById("counter");
 let sum=0;
-function incrementBtn(){
+
+let btntag=document.getElementById("incrementBtn");
+btntag.addEventListener("click",()=>{
 	sum++;
 	alert(sum);
 	ptag.textContent=sum;
-	
-	
-}
+})
